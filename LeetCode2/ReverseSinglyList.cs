9d56793376f2b4843c0ec9305a17f43425fbe2f9
Bhavik Gosrani <bhavik4748@gmail.com>
2018-04-29 14:07:55 -0400

    Reverse List with O(1)

--- LeetCode2/ReverseSinglyList.cs
+++ LeetCode2/ReverseSinglyList.cs
@@ -9,10 +9,33 @@ namespace LeetCode2
      * 
      * 
      *Reverse a singly linked list. 
+     * 1->2->3->null to 3->2->1->null
      * 
      * */
     public partial class Program
     {
+        public static ListNode WithoutStackUseReverseList(ListNode head)
+        {
+
+            if (head == null)
+                return head;
+
+            ListNode current = head;
+            ListNode prev = null;
+
+            while(current != null)
+            {
+                ListNode temp = current.next;
+                current.next = prev;
+                prev = current;
+                current = temp;
+            }
+
+
+            return prev;
+
+        }
+
         public static ListNode ReverseList(ListNode head)
         {
 
