23040682c0b91db2a44e5946916f653976e60dfb
Bhavik Gosrani <bhavik4748@gmail.com>
2018-08-04 16:05:05 -0400

    New Problems added

---
+++ LeetCode2/LongestPalindrome.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode2
+{
+    partial class Program
+    {
+        public static string LongestPalindrome(string s)
+        {
+            if (s.Length < 1)
+                return "";
+            int start = 0, end = 0; ;
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                int len1 = HelperPalin(s, i, i);
+                int len2 = HelperPalin(s, i, i + 1);
+
+                int len = Math.Max(len1, len2);
+                if (len > end - start)
+                {
+                    start = i - (len - 1) / 2;
+                    end = i + len / 2;
+                }
+            }
+
+            return JavaStyleSubstring(s, start, end + 1);
+        }
+
+
+
+        static int HelperPalin(string s, int start, int end)
+        {
+            int L = start;
+            int R = end;
+
+            while (L >= 0 && R < s.Length && s[L] == s[R])
+            {
+                L--;
+                R++;
+            }
+
+            return R - L - 1;
+        }
+
+        static string JavaStyleSubstring(string s, int beginIndex, int endIndex)
+        {
+            // simulates Java substring function
+            int len = endIndex - beginIndex;
+            return s.Substring(beginIndex, len);
+        }
+    }
+}
---
+++ LeetCode2/MaxAreaofIsland.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode2
+{
+    partial class Program
+    {
+        public static int MaxAreaOfIsland(int[,] grid)
+        {
+
+            int x = grid.GetLength(0);
+            int y = grid.GetLength(1);
+            bool[,] seen = new bool[x, y];
+            int res = 0;
+            for (int i = 0; i < x; ++i)
+            {
+                for (int j = 0; j < y; ++j)
+                {
+                    int r = 0;
+                    if (grid[i, j] == 1)
+                    {
+                        r = HelperFunction(grid, i, j, x, y, 0, seen);
+                        res = Math.Max(r, res);
+                    }
+
+                }
+            }
+            return res;
+        }
+
+        static int HelperFunction(int[,] grid, int i, int j, int x, int y, int area, bool[,] seen)
+        {
+
+            //if (grid[i, j] == 1)
+            //{
+            //    ++area;
+            //   // grid[i, j] = 0;
+            //}
+
+            if (i  >= x || j  >= y || i  < 0 || j < 0 || seen[i,j] ||  grid[i, j] == 0)
+                return 0;
+
+            seen[i, j] = true;
+
+            area = 1+ HelperFunction(grid, i + 1, j, x, y, area, seen) + HelperFunction(grid, i, j + 1, x, y, area, seen) + HelperFunction(grid, i - 1, j, x, y, area,seen) + HelperFunction(grid, i, j - 1, x, y, area,seen);
+
+            return area;
+
+
+        }
+    }
+}
